dd0712fcf9a352cdd419d0fe985d8f3ec96f0051
rpantyushin <rpantyushin25@gmail.com>
2024-08-29 20:52:12 +0300

    Add files via upload

---
+++ 2lessonPEREMENNIYE.py
@@ -0,0 +1,5 @@
+Homework_count = 12
+Hour_count = 1.5
+Course_name = "Python"
+Average_time_per_task = (Hour_count / Homework_count)
+print ("Курс:", Course_name, ", всего задач:", Homework_count, ", затрачено часов:", Hour_count, ", среднее время выполнения", Average_time_per_task )
